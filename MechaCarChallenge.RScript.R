955279d9ac9a0784e3a29d105d3b18c01d65ddd4
christinepuri <cibrahimpuri@fordham.edu>
2021-10-12 13:28:14 -0400

    Adding MechaCar and Suspension Coil Code

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -4,3 +4,12 @@ mechacar <- read.csv(file = 'MechaCar_mpg.csv',
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle +
              ground_clearance + AWD,data =  mechacar))
+
+suspcoil <- read.csv(file = 'Suspension_Coil.csv', 
+                     check.names =  F, stringsAsFactors = F)
+total_summary <- summarise(suspcoil, Mean = mean(PSI), Median =median(PSI), 
+                           Variance = var(PSI), SD = sd(PSI))
+
+group_lot <- group_by(suspcoil, Manufacturing_Lot)
+lot_summary <- summarise(group_lot, Mean = mean(PSI), Median =median(PSI), 
+                         Variance = var(PSI), SD = sd(PSI) )
