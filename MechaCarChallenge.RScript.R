c3a87e57b298f32e5e2bf11a96012d870a87931b
christinepuri <cibrahimpuri@fordham.edu>
2021-11-09 13:25:19 -0500

    Updating code

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -13,3 +13,19 @@ total_summary <- summarise(suspcoil, Mean = mean(PSI), Median =median(PSI),
 group_lot <- group_by(suspcoil, Manufacturing_Lot)
 lot_summary <- summarise(group_lot, Mean = mean(PSI), Median =median(PSI), 
                          Variance = var(PSI), SD = sd(PSI) )
+
+# T-Tests on Suspension Coils
+t.test(suspcoil$PSI, mu = 1500)
+
+# T-test lot 1 
+plot1 <- subset(suspcoil, Manufacturing_Lot == "Lot1")
+t.test(plot1$PSI, mu = 1500)
+
+#T-test lot 2
+plot2 <- subset(suspcoil, Manufacturing_Lot == "Lot2")
+t.test(plot2$PSI, mu = 1500)
+
+#T-test lot 3 
+plot3 <- subset(suspcoil, Manufacturing_Lot == "Lot3")
+t.test(plot3$PSI, mu = 1500)
+
